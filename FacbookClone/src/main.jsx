d4052c226a5406ca53d789bebddb5208ed91a941
Kyle8Bits <khoamaidang2611@gmail.com>
2024-08-19 16:57:02 +0700

    post component

--- FacbookClone/src/main.jsx
+++ FacbookClone/src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
+import Post from './components/Post'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-
+    <Post/>
   </React.StrictMode>,
 )
