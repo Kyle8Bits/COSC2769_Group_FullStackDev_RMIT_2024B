3f6853106a306ad1aa8236d46936fe8bb5abe56c
Kyle8Bit <khoamaidang2611@gmail.com>
2024-08-20 00:11:58 +0700

    fix cònlict

---
+++ FacbookClone/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './Header';
+import Login from './pages/Login';
+
+
+const App = () => {
+    return (
+        <div className="app">
+            <Header />
+            <Login />
+        </div>
+    );
+};
+
+export default App;
