02df1b796962ad5ef314c6255d430311860a8dcc
Kyle8Bit <khoamaidang2611@gmail.com>
2024-08-19 23:36:20 +0700

    delete conflict

--- FacbookClone/src/pages/GroupFeed.jsx
+++ FacbookClone/src/pages/GroupFeed.jsx
@@ -5,32 +5,8 @@ import banner from '../assets/banner.png'
 
 function GroupFeed() {
   return (
-    <div className='group_container'>
-      {/* <div className="group_banner">
-        <img src={banner} alt="" />
-      </div> */}
-
-      <div className="group_info">
-
-        <div>
-          <h1 className="name">Software Engineering Community</h1>
-
-          <div className="property">
-              <h5>50k members - Private <i class="ri-lock-line"></i></h5>
-          </div>
-        </div>
-
-        <div className='button'>
-          <button className="join">  <i class="ri-login-box-line"></i> Join </button>
-          <button className="invite"> <i class="ri-add-box-line"></i> Invite</button>
-          <button className="setting"> <i class="ri-tools-fill"></i> Admin</button>
-        </div>
-
-      </div>
-      <div className="group_feed">
-        <Post/>
-        <Post/>
-      </div>
+    <div >
+    
     </div>
   )
 }
