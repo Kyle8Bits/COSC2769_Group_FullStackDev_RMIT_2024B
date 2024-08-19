7f48def9ddeb864ad32454b14b55e0ad0ad3d704
ctungnguyen <s3976069@rmit.edu.vn>
2024-08-20 03:40:32 +0700

    Login page update

--- FacbookClone/src/App.jsx
+++ FacbookClone/src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import Header from './Header';
+
 import Login from './pages/Login';
 
 
 const App = () => {
     return (
         <div className="app">
-            <Header />
+            
             <Login />
         </div>
     );
--- FacbookClone/src/pages/Login.jsx
+++ FacbookClone/src/pages/Login.jsx
@@ -67,39 +67,38 @@ const Login = () => {
   // );
 
   return (
-    <div className="Login">
-      <div className="facebook">
-        <div className="facebooktext">Facebook</div>
-        <div className="title">
-          Facebook helps you connect and share <br></br>with the people in your
-          life.
+    <div className="login_page">
+        
+        <div className="login_container">
+
+            {/* login form, left side */}
+            <div className="login_detail">
+                <h2>Login</h2>
+                <input type="email" placeholder="Email address or Phone Number" />
+                <input type="Password" placeholder="Password" />
+                <button className="log_in_button">
+                  {/* <Link to="/HeaderArea">LogIn</Link> */}
+                </button>
+            </div>
+
+            <div className="forget">
+                <a href="forget_button">Forgotten account?</a>
+
+            </div>
+            <div className="create">
+
+                <button className="create_new_account_button">Create New Account</button>
+            </div>
         </div>
-      </div>
-      <div className="loginContainer">
-        <div className="logindetail">
-          <input type="email" placeholder="Email address or Phone Number" />
-          <br></br>
-          <input type="Password" placeholder="Password" />
-          <br></br>
-          <button className="btn">
-            {/* <Link to="/HeaderArea">LogIn</Link> */}
-          </button>
+
+
+        {/* right side */}
+        <div className="welcome_container">
+            {/* The application title */}
+            <p className="welcome_text">Welcome to</p>
+            <h2 className="title">CrabNest</h2>
+            <p className="intro_text">This is a social app can help to connect friends and widen/create your comminity</p>
         </div>
-        <div className="forget">
-          <a href="forget">Forgotten account?</a>
-          <br></br>
-        </div>
-        <div className="create">
-          <br></br>
-          <button className="btns">Create New Account</button>
-        </div>
-        <p></p>
-        <br></br>
-        <div className="page">
-          <a href="createpage">Create a Page </a> for a celebrity, band or
-          business.
-        </div>
-      </div>
     </div>
   );
 };
