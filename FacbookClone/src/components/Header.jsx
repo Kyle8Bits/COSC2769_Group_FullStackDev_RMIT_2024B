785645bfcc874dff0c603f2c992b1510b1f8b285
Kyle8Bits <153161848+Kyle8Bits@users.noreply.github.com>
2024-08-20 19:43:31 +0700

    Add files via upload

--- FacbookClone/src/components/Header.jsx
+++ FacbookClone/src/components/Header.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
+import logo from '../assets/logo.png'
+import avatar from '../assets/avatar.jpg'
+import '../css/header.css'
 
 function Header() {
   return (
     <div className='header_container'>
-        
-      
+
+        <div className="search_bar">
+            <img src={logo} alt="" />
+            <div className="input_box">
+                <i class="ri-search-line"></i>
+                <input type="text" placeholder='Search CrabNest' />
+            </div>
+        </div>
+
+        <div className="header_nav">
+        <i class="ri-home-5-fill"></i>
+        <i class="ri-team-fill"></i>
+        <i class="ri-group-2-fill"></i>
+        <i class="ri-tools-fill"></i>
+        </div>
+    
+        <div className="noti_action">
+        <i class="ri-notification-4-fill"></i>
+        <img src={avatar} alt="" />
+        </div>
     </div>
   )
 }
