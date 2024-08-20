8f7d1c7b86deb1febc107a1f37e32b21b0b3f8c8
Kyle8Bits <khoamaidang2611@gmail.com>
2024-08-20 21:01:14 +0700

    fix

--- FacbookClone/src/main.jsx
+++ FacbookClone/src/main.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Header from './components/Header'
-
-
+import GroupFeed from './pages/GroupFeed'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Header/>
-    
+    <GroupFeed/>
   </React.StrictMode>,
 )
