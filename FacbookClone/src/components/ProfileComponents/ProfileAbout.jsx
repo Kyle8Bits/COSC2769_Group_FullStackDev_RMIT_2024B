ddeed49dd0ac1642cb52058412fff9a413609683
Kyle8Bits <khoamaidang2611@gmail.com>
2024-08-21 21:59:40 +0700

    ds

--- FacbookClone/src/components/ProfileComponents/ProfileAbout.jsx
+++ FacbookClone/src/components/ProfileComponents/ProfileAbout.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 function ProfileAbout() {
   return (
-    <div >
-
+    <div  className='profile_about_container'>
+      <div className="about_content">
+        <div className="personal">
+          <h3> <i class="ri-graduation-cap-fill"></i> Study at RMIT University</h3>
+          <h3> <i class="ri-briefcase-4-fill"></i> Work at Space X</h3>
+          <h3> <i class="ri-briefcase-4-fill"></i>  Works at RMIT Student Ambassadors Team - S.A.T</h3>
+          <h3><i class="ri-phone-fill"></i>  09123456789</h3>
+        </div>
+      </div>
+      <div className="space"></div>
     </div>
   )
 }
---
+++ FacbookClone/src/pages/NewFeed.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NewFeed() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default NewFeed
