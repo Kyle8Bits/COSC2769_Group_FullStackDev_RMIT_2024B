36662e7000ce3900b5e0925bb1164e13c5036a59
Kyle8Bit <khoamaidang2611@gmail.com>
2024-08-22 04:42:45 +0700

    showcase

--- FacbookClone/src/components/ProfileComponents/ProfilePosts.jsx
+++ FacbookClone/src/components/ProfileComponents/ProfilePosts.jsx
@@ -14,7 +14,6 @@ function ProfilePosts() {
       <ul className='profile_post_list'>
         <li><Post author_avatar={avatar} author_name={"Mai Dang Khoa"} photo={post4} caption={"So they really make you build a Facebook"}/></li>
         <li><Post author_avatar={avatar} author_name={"Mai Dang Khoa"} photo={post5} caption={"Damn, my friends are exhausted from deadlines."}/></li>
-        <li><Post/></li>
       </ul>
     </div>
   )
