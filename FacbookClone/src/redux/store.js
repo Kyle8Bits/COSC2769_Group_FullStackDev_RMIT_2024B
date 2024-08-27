9ec13bc1950cc8d8cd340221f800e437f0fb2c74
Kyle8Bit <khoamaidang2611@gmail.com>
2024-08-28 00:17:51 +0700

    register slice

--- FacbookClone/src/redux/store.js
+++ FacbookClone/src/redux/store.js
@@ -3,14 +3,15 @@ import commentReducer from './slice/commentsSlice';
 import loginReducer from './slice/loginSlice'
 import profileReducer from './slice/profileSlice';
 import friendReducer from './slice/friendSlice'
-import basicInfoReducer from './slice/basicInfoSlice'
+import registerReducer from "./slice/registerSlice";
+
 export const store = configureStore({
     reducer:{
         comment: commentReducer,
         auth: loginReducer,
         profile: profileReducer,
         friends: friendReducer,
-        basicInfo: basicInfoReducer,
+        register: registerReducer
     }
     
 })
