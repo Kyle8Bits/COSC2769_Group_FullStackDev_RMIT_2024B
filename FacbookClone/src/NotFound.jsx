8c53a8f27ec539475ca0fd77812d4242ee4cd6c0
ctungnguyen <s3976069@rmit.edu.vn>
2024-09-04 01:28:23 +0700

    notfound page

--- FacbookClone/src/NotFound.jsx
+++ FacbookClone/src/NotFound.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from './image/logo.png';
+import './css/notfound.css';
+import Header from './components/shared/Header'
 
-function NotFound() {
+const NotFound = () => {
   return (
-    <div style={{color:'white'}}>NotFound</div>
-  )
+    <>
+    <Header/>
+    <div className="notfound-container">
+      <div className="logo_and_text_container">
+        <div className="logo_img">
+          <img src={logo} alt="Logo" />
+        </div>
+        <div>
+          <h1 className="notfound-title">404</h1>
+          <p className="notfound-message">Oops! The page you're looking for doesn't exist.</p>
+          <Link to="/home" className="notfound-link">
+            <button className="notfound-button">Go Home</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+    </>
+  );
 }
 
-export default NotFound
+export default NotFound;
